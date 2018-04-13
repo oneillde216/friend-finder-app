c29c4dad6d3982d62f0f34be45d5c489bb6b00c5
oneillde216 <31212974+oneillde216@users.noreply.github.com>
2018-04-13 11:51:30 -0400

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Lyndon B Johnson",
+		photo: "https://cdn.history.com/sites/2/2013/12/lbj-color-AB.jpeg",
+		scores: [5, 5, 3, 1, 2, 2, 4, 1, 1, 1]
+	},
+	{
+		name: "Jimmy Carter",
+		photo: "https://www.history.com/topics/us-presidents/jimmy-carter/pictures/jimmy-carter",
+		scores: [3, 3, 5, 5, 1, 2, 3, 2, 1, 1]
+	},
+	{
+		name: "Ronald Reagan",
+		photo: "https://cdn.history.com/sites/2/2013/12/reagan_flags-AB.jpeg",
+		scores: [1, 1, 2, 3, 5, 5, 2, 2, 1, 1]
+	},
+	{
+		name: "Barack Obama",
+		photo: "https://cdn.history.com/sites/2/2013/11/obama_color-AB.jpeg",
+		scores: [1, 4, 1, 2, 4, 1, 5, 5, 1, 1]
+	},
+	{
+		name: "Donald Trump",
+		photo: "https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_400x400.jpg",
+		scores: [1, 4, 1, 4, 3, 2, 1, 2, 5, 5]
+	}
+];
+
+module.exports = friends;
